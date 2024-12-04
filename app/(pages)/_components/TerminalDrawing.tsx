385ba33d24c074a3e5e6b491afb1a4edd29dd04e
xcute <xcute.official@gmail.com>
2024-12-04 13:23:03 -0500

    testing

---
+++ app/(pages)/_components/TerminalDrawing.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TerminalDrawing = () => {
+  return (
+    <div className='border rounded-md p-2'>
+        <div className='text-sm'>
+            <span>(base) palak@machine:~/anjali/palak/playground/root/frontends/root-frontends-main$ <span>figlet popsypops</span></span>
+            
+        </div>
+    </div>
+  )
+}
+
+export default TerminalDrawing
--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import TerminalDrawing from "./_components/TerminalDrawing"
+
 
 const page = () => {
   return (
-    <div>home</div>
+    <div>
+      <section>
+        <div className="flex items-center">
+          <div className="font-bold mt-32">
+            <span className="text-sm">Hi, I am</span>
+            <h1 className='text-6xl'>
+              <span className="">Popsy </span>
+              <span>pops, software Developer and CS enthusiast</span>
+            </h1>
+            <span>I develop websites, software tailored to customer needs, we are professional in creating digital markets and networks for growing businesses.</span>
+          </div>
+          <div>
+            <TerminalDrawing />
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
